30bceffd31fdaaa095dfb3fae728214e800415c7
JohnF <johnfrades@gmail.com>
2024-10-04 15:39:59 +0800

    feat: add truncate on header title

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,9 @@ import DarkModeToggle from '@/components/dark-mode-toggle';
 const Header = () => {
   return (
     <header className='flex items-center justify-between py-4 px-8 bg-white dark:bg-gray-800 shadow-[0_2px_4px_rgba(0,0,0,0.1)] dark:shadow-[0_2px_4px_rgba(255,255,255,0.1)]'>
-      <h1 className='text-xl font-bold text-gray-900 dark:text-white'>Data Dashboard Discovery</h1>
+      <h1 className='text-xl font-bold text-gray-900 dark:text-white truncate pr-3'>
+        Data Dashboard Discovery
+      </h1>
       <div className='flex items-center space-x-4'>
         <Link
           href='https://github.com/johnfrades'
